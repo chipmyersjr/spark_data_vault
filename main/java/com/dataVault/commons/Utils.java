ae44b0429b4fdb9eab427eb5ebdeffdeae419137
Chip <chip.myersjr@gmail.com>
2019-06-12 12:57:44 -0700

    creating generic function for updating hub tables

---
+++ main/java/com/dataVault/commons/Utils.java
@@ -0,0 +1,79 @@
+package com.dataVault.commons;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+
+import java.io.File;
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.apache.spark.sql.functions.*;
+
+public class Utils {
+    public static void updateHubTable(SparkSession session, Dataset<Row> newIdsDataset, String hubTableName
+                                      , String idColumnName, String businessKeyColumnName, String recordSource
+                                      , String hashKeyColumnName) {
+        /*
+        Generic function to update a hub table given a distinct list of business keys.  Will insert new hub record for
+        all keys that don't currently exist in the hub tables
+
+        session: SparkSession object to perform operations
+        newIdsDataset: Distinct list of newly arrived business keys
+        hubTableName: the name of the hub table
+        idColumnName: column name of the key column in newIdsDataset
+        businessKeyColumnName: business key column name for hub table
+        recordSource: record source field value for hub table
+        hashKeyColumnName: the name of the hash key column in the hub table
+        * */
+
+        Dataset<Row> ids_to_update;
+        String hub_dir = "out/" + hubTableName;
+        File dir = new File(hub_dir);
+
+        if (dir.exists()){
+            Dataset<Row> existing_ids = session.read().parquet(hub_dir + "/*/*/*/*/*/*/");
+
+            newIdsDataset.registerTempTable("new");
+
+            existing_ids.registerTempTable("existing");
+
+            ids_to_update = session.sql("SELECT " + idColumnName +
+                    " FROM new " +
+                    " WHERE + " + idColumnName +
+                    " NOT IN (SELECT " +  businessKeyColumnName + " FROM existing)");
+        } else {
+            ids_to_update = newIdsDataset.select(idColumnName);
+        }
+
+        session.udf().register("getMd5Hash", (String x) -> getMd5Hash(x), DataTypes.StringType);
+
+        Dataset<Row> hub_data = ids_to_update.withColumn(hashKeyColumnName, callUDF("getMd5Hash", col(idColumnName)))
+                .withColumn(businessKeyColumnName, col(idColumnName))
+                .withColumn("record_source", lit(recordSource))
+                .withColumn("created_at", current_timestamp() )
+                .drop(idColumnName);
+
+        String date = new SimpleDateFormat("yyyy/MM/dd/HH/mm/ss").format(new Date());
+
+        hub_data.repartition(1).write().mode("overwrite").parquet(hub_dir + "/" + date);
+    }
+
+    private static String getMd5Hash(String business_key) throws Exception {
+        /*
+        Returns MD5 hash for given business_key
+
+        business_key: business key to hash
+        * */
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(business_key.getBytes());
+        byte[] digest = md.digest();
+        StringBuilder sb = new StringBuilder();
+        for (byte b : digest) {
+            sb.append(String.format("%02x", b & 0xff));
+        }
+        return sb.toString();
+    }
+}
