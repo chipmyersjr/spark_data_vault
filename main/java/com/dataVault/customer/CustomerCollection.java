2141ff3a1863067df62da1263c3f8376ac5fd58a
Chip <chip.myersjr@gmail.com>
2019-06-30 13:35:51 -0700

    email hub

--- main/java/com/dataVault/customer/CustomerCollection.java
+++ main/java/com/dataVault/customer/CustomerCollection.java
@@ -16,11 +16,11 @@ public class CustomerCollection {
     * */
     public static void main(String[] args) {
 
-        //System.setProperty("hadoop.home.dir", "C:/hadoop");
+        System.setProperty("hadoop.home.dir", "C:/hadoop");
         Logger.getLogger("org").setLevel(Level.ERROR);
-        SparkSession session = SparkSession.builder().appName("customerCollection").getOrCreate();
+        SparkSession session = SparkSession.builder().appName("customerCollection").master("local[1]").getOrCreate();
 
-        Dataset<Row> customers = session.read().json("s3n://chip-data-vault/in/customer_collection.json");
+        Dataset<Row> customers = session.read().json("in/customer_collection.json");
 
         Dataset<Row> distinct_customers = customers.select("_id").distinct().filter("_id is not null");
 
@@ -29,9 +29,18 @@ public class CustomerCollection {
                 ,  "customer_hash_key");
 
         Dataset<Row> sat_customer_collection_ds = customers.filter("_id is not null")
-                .drop("confirmation_token", "confirmation_token_expires_at", "password_hash");
+                .drop("confirmation_token", "confirmation_token_expires_at", "password_hash", "emails");
 
         Utils.updateSatTable(session, sat_customer_collection_ds, "_id", "customer_hash_key"
                 , "sat_customer_collection", "app_customer_collection");
+
+        customers.filter("_id is not null").registerTempTable("customers");
+        Dataset<Row> emails = session.sql("SELECT emails_e.* FROM customers LATERAL VIEW explode(emails) as emails_e");
+
+        Dataset<Row> distinct_email_ids = emails.select("_id").distinct();
+
+        Utils.updateHubTable(session, distinct_email_ids, "hub_email", "_id"
+                , "email_internal_application_id", "app_customer_collection"
+                ,  "email_hash_key");
     }
 }
